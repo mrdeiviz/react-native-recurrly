43e54624cbd1fe92ef9bfe3c3ca46de1e60bdbcb
David Ramos <david.rfortes@gmail.com>
2026-04-03 16:26:51 +0200

    implement fonts

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -2,13 +2,13 @@ import { Link } from 'expo-router'
 import React from 'react'
 import { Text, View } from 'react-native'
 
-const SingIn = () => {
+const SignIn = () => {
   return (
     <View>
-      <Text>SingIn</Text>
+      <Text>SignIn</Text>
       <Link href='/(auth)/sign-up'>Create Account</Link>
     </View>
   )
 }
 
-export default SingIn
+export default SignIn
